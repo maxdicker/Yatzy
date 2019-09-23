103b22fa3edfe6fa574fb432a0046177b4a9652b
max.dicker <max.dicker@myob.com>
2019-09-23 14:26:49 +1000

    Pass 'Yatzy' test

--- src/main/java/com/max/ScoreCalculator.java
+++ src/main/java/com/max/ScoreCalculator.java
@@ -5,4 +5,8 @@ public class ScoreCalculator {
     public int calculateChance(int dice1, int dice2, int dice3, int dice4, int dice5) {
         return dice1+dice2+dice3+dice4+dice5;
     }
+
+    public int calculateYatzy(int i, int i1, int i2, int i3, int i4) {
+        return 0;
+    }
 }
