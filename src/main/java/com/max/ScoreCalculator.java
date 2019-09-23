6983555dcb765c51dcd55425e4ac42b31a9774b7
max.dicker <max.dicker@myob.com>
2019-09-23 14:22:24 +1000

    Pass 'Chance' test

--- src/main/java/com/max/ScoreCalculator.java
+++ src/main/java/com/max/ScoreCalculator.java
@@ -2,4 +2,7 @@ package com.max;
 
 public class ScoreCalculator {
 
+    public int calculateChance(int dice1, int dice2, int dice3, int dice4, int dice5) {
+        return dice1+dice2+dice3+dice4+dice5;
+    }
 }
