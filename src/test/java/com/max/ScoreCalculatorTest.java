231a86588489478e3979039b8a33cc6157f39d25
max.dicker <max.dicker@myob.com>
2019-09-23 14:29:51 +1000

    New 'Yatzy' test

--- src/test/java/com/max/ScoreCalculatorTest.java
+++ src/test/java/com/max/ScoreCalculatorTest.java
@@ -7,16 +7,23 @@ import org.junit.Test;
 public class ScoreCalculatorTest {
 
     @Test
-    public void calculateChance()
+    public void chance()
     {
         ScoreCalculator calculator = new ScoreCalculator();
         assertEquals(15, calculator.calculateChance(1,2,3,4,5));
     }
 
     @Test
-    public void calculateYatzy()
+    public void incompleteYatzyReturns0()
     {
         ScoreCalculator calculator = new ScoreCalculator();
         assertEquals(0, calculator.calculateYatzy(1,1,1,2,1));
+    }
+
+    @Test
+    public void YatzyReturns50()
+    {
+        ScoreCalculator calculator = new ScoreCalculator();
+        assertEquals(50, calculator.calculateYatzy(1,1,1,1,1));
     }
 }
