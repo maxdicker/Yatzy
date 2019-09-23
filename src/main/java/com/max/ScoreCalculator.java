eaf4313913cbdedc0f8d85498b5608cf25525a16
max.dicker <max.dicker@myob.com>
2019-09-23 16:04:54 +1000

    Change methods to accept an array

--- src/main/java/com/max/ScoreCalculator.java
+++ src/main/java/com/max/ScoreCalculator.java
@@ -2,12 +2,16 @@ package com.max;
 
 public class ScoreCalculator {
 
-    public int calculateChance(int d1, int d2, int d3, int d4, int d5) {
-        return d1+d2+d3+d4+d5;
+    public int calculateChance(int[] die) {
+        int score = 0;
+        for (int i : die) {
+            score += i;
+        }
+        return score;
     }
 
-    public int calculateYatzy(int d1, int d2, int d3, int d4, int d5) {
-        if (d1==d2 && d2==d3 && d3==d4 && d4==d5) {
+    public int calculateYatzy(int[] die) {
+        if (die[0]==die[1] && die[1]==die[2] && die[2]==die[3] && die[3]==die[4]) {
             return 50;
         } else {
             return 0;
