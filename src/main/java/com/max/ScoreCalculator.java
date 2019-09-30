aca47e2df71e6c822be3ff4ad9833e2db8a89e11
max.dicker <max.dicker@myob.com>
2019-09-30 15:34:16 +1000

    Change methods to use frequency Map.

--- src/main/java/com/max/ScoreCalculator.java
+++ src/main/java/com/max/ScoreCalculator.java
@@ -1,6 +1,5 @@
 package com.max;
 
-//Use hashmap for everything
 //Once done, break calculations out into many subclasses. Will make game controlling easier?
 import java.util.*;
 
@@ -15,7 +14,7 @@ public class ScoreCalculator {
     }
 
     private TreeMap<Integer, Integer> toFrequencyMap(List<Integer> values) {
-        TreeMap<Integer, Integer> frequenciesByValue = new TreeMap<>();
+        TreeMap<Integer, Integer> frequenciesByValue = new TreeMap<>(Collections.reverseOrder());
         for (int value : values) {
             frequenciesByValue.put(value, frequenciesByValue.getOrDefault(value, 0) + 1);
         }
@@ -24,8 +23,8 @@ public class ScoreCalculator {
 
     private int sum(Map<Integer, Integer> frequenciesByValue) {
         int sum = 0;
-        for (int value : frequenciesByValue.keySet()) {
-            sum += (value * frequenciesByValue.get(value));
+        for (Map.Entry<Integer, Integer> entry : frequenciesByValue.entrySet()) {
+            sum += (entry.getKey() * entry.getValue());
         }
         return sum;
     }
@@ -73,90 +72,77 @@ public class ScoreCalculator {
     }
 
     public int calculatePair(List<Integer> values) {
-        Map<Integer, Integer> frequenciesByValue = toFrequencyMap(values);
-        return findHighestPair(frequenciesByValue) * 2;
-    }
-
-    private int findHighestPair(Map<Integer, Integer> frequenciesByValue) {
-        List<Integer> pairs = findPairs(frequenciesByValue);
-        Collections.reverse(pairs);
-        return pairs.get(0);
-    }
-
-    private ArrayList<Integer> findPairs(Map<Integer, Integer> frequenciesByValue) {
-        ArrayList<Integer> pairs = new ArrayList<>();
-
-        for (int value : frequenciesByValue.keySet()) {
-            if (frequenciesByValue.get(value) > 1) {
-                pairs.add(value);
-            }
-        }
-
-        return pairs;
-    }
-
-    public int calculateTwoPairs(List<Integer> values) {
-        Map<Integer, Integer> frequenciesByValue = toFrequencyMap(values);
-        List<Integer> pairs = findPairs(frequenciesByValue);
-
-        if (pairs.size() == 2) {
-            return (pairs.get(0) + pairs.get(1)) * 2;
-        } else {
-            return 0;
-        }
+        return calculateXOfAKind(2, values);
     }
 
     public int calculateThreeOfAKind(List<Integer> values) {
         return calculateXOfAKind(3, values);
     }
 
+    public int calculateFourOfAKind(List<Integer> values) {
+        return calculateXOfAKind(4, values);
+    }
+
     private int calculateXOfAKind(int number, List<Integer> values) {
         Map<Integer, Integer> frequenciesByValue = toFrequencyMap(values);
 
-        for (int value : frequenciesByValue.keySet()) {
-            if (frequenciesByValue.get(value) >= number) {
-                return value * number;
+        for (Map.Entry<Integer, Integer> entry : frequenciesByValue.entrySet()) {
+            if (entry.getValue() >= number) {
+                return entry.getKey() * number;
             }
         }
 
         return 0;
     }
 
-    public int calculateFourOfAKind(List<Integer> values) {
-        return calculateXOfAKind(4, values);
+    public int calculateTwoPairs(List<Integer> values) {
+        Map<Integer, Integer> frequenciesByValue = toFrequencyMap(values);
+        int score = 0;
+        int numPairsSeen = 0;
+
+        for (Map.Entry<Integer, Integer> entry : frequenciesByValue.entrySet()) {
+            if (entry.getValue() >= 2) {
+                score += entry.getKey();
+                numPairsSeen ++;
+            }
+            if (numPairsSeen == 2) {
+                return score * 2;
+            }
+        }
+
+        return 0;
     }
 
     public int calculateSmallStraight(List<Integer> values) {
         return calculateStraight(smallStraight, values);
     }
 
-    private int calculateStraight(List<Integer> straightType, List<Integer> values) {
-        List<Integer> sortedValues = new ArrayList<>(values);
-        Collections.sort(sortedValues);
-
-        if (sortedValues.equals(straightType)) {
-            Map<Integer, Integer> frequenciesByValue = toFrequencyMap(sortedValues);
-            return sum(frequenciesByValue);
-        } else {
-            return 0;
-        }
-    }
-
     public int calculateLargeStraight(List<Integer> values) {
         return calculateStraight(largeStraight, values);
     }
 
-    public int calculateFullHouse(List<Integer> values) {
-        if (isFullHouse(values)) {
-            Map<Integer, Integer> frequenciesByValue = toFrequencyMap(values);
+    private int calculateStraight(List<Integer> straightDefinition, List<Integer> values) {
+        Map<Integer, Integer> frequenciesByValue = toFrequencyMap(values);
+        Map<Integer, Integer> straight = toFrequencyMap(straightDefinition);
+
+        if (frequenciesByValue.equals(straight)) {
             return sum(frequenciesByValue);
         } else {
             return 0;
         }
     }
 
-    private boolean isFullHouse(List<Integer> values) {
+    public int calculateFullHouse(List<Integer> values) {
         Map<Integer, Integer> frequenciesByValue = toFrequencyMap(values);
+
+        if (isFullHouse(frequenciesByValue)) {
+            return sum(frequenciesByValue);
+        } else {
+            return 0;
+        }
+    }
+
+    private boolean isFullHouse(Map<Integer, Integer> frequenciesByValue) {
         return frequenciesByValue.containsValue(2) && frequenciesByValue.containsValue(3);
     }
 }
